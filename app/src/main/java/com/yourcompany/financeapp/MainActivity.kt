ce8538f0545c07f818310e452520c6168525c7fe
L-muchai <muchlewi@gmail.com>
2025-12-23 22:28:50 +0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/yourcompany/financeapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.yourcompany.financeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.yourcompany.financeapp.presentation.screens.home.HomeScreen
+import com.yourcompany.financeapp.presentation.theme.FinanceAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FinanceAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomeScreen()
+                }
+            }
+        }
+    }
+}
