83943ab61484a0aeb58408a8bb17b6d4edda5d55
L-muchai <muchlewi@gmail.com>
2025-12-23 23:06:52 +0300

    Update MainActivity.kt

--- app/src/main/java/com/yourcompany/financeapp/MainActivity.kt
+++ app/src/main/java/com/yourcompany/financeapp/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.yourcompany.financeapp.presentation.screens.home.HomeScreen
+import com.yourcompany.financeapp.presentation.navigation.NavGraph
 import com.yourcompany.financeapp.presentation.theme.FinanceAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    HomeScreen()
+                    NavGraph()
                 }
             }
         }
